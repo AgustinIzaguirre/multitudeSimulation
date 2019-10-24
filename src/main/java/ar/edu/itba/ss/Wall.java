68fb5023e7781dbf61ce9dc8c1db287f5230f1a4
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-10-24 11:09:51 -0300

    Wall updated to radius definition

--- src/main/java/ar/edu/itba/ss/Wall.java
+++ src/main/java/ar/edu/itba/ss/Wall.java
@@ -7,19 +7,11 @@ import javafx.geometry.Point2D;
 
 
 public class Wall {
-    private Point2D start;
-    private Point2D end;
+    private double radius;
 
-    public Wall(Point2D point1, Point2D point2) throws InvalidArgumentException {
-        if ((point1.getY() == point2.getY()) && (point1.getX() < point2.getX()) ||
-                (point1.getX() == point2.getX()) && (point1.getY() < point2.getY())) {
-            start = point1;
-            end   = point2;
-        }
-        else if((point1.getY() == point2.getY()) && (point1.getX() > point2.getX()) ||
-                (point1.getX() == point2.getX()) && (point1.getY() > point2.getY())) {
-            start = point2;
-            end   = point1;
+    public Wall(double radius) throws InvalidArgumentException {
+        if (radius > 0) {
+            this.radius = radius;
         }
         else {
             throw new InvalidArgumentException(new String[]{"There is no wall"});
@@ -27,20 +19,9 @@ public class Wall {
     }
 
     public Point2D getDistance(Particle particle) {
-        if(start.getX() == end.getX() &&
-                start.getY() < particle.getPosition().getY() &&
-                particle.getPosition().getY() < end.getY()) {
-            return new Point2D(particle.getPosition().getX() - start.getX(), 0);
-        }
-        else if(start.getY() == end.getY() &&
-                start.getX() < particle.getPosition().getX() &&
-                particle.getPosition().getX() < end.getX()) {
-            return new Point2D(0, particle.getPosition().getY() - start.getY());
-        }
-        else {
-            Point2D distanceToStart = start.subtract(particle.getPosition());
-            Point2D distanceTOEnd   = end.subtract(particle.getPosition());
-            return (distanceTOEnd.magnitude() < distanceToStart.magnitude()) ? distanceTOEnd.multiply(-1) : distanceToStart.multiply(-1);
-        }
+        double particleCenterDistance = particle.getPosition().magnitude();
+        double distance = Math.abs(radius - particleCenterDistance);
+        Point2D normalVersor = particle.getPosition().normalize();
+        return normalVersor.multiply(distance);
     }
 }
