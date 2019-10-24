6a9cec4cd105096ae9a7fc5f4413a7d77167e721
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-10-24 15:14:36 -0300

    circle Drawer added

---
+++ CircleDrawer.py
@@ -0,0 +1,24 @@
+﻿import ovito
+import numpy as np
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import ovito.vis
+
+
+
+def render(painter, **args):
+	# Draw external circle
+	pen = QPen(Qt.SolidLine)
+	pen.setWidth(2)
+	pen.setColor(QColor(255,255,255))
+	painter.setPen(pen)
+	externalRadius = 110
+	painter.drawEllipse(QPointF(312, 246),externalRadius , externalRadius)
+
+	# Draw internal circle
+	pen = QPen(Qt.SolidLine)
+	pen.setWidth(2)
+	pen.setColor(QColor(255,255,255))
+	painter.setPen(pen)
+	internalRadius = 50
+	painter.drawEllipse(QPointF(312, 246),internalRadius , internalRadius)
